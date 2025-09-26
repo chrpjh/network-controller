bb26d8d6795ff339cf235fa6c12719c93adb906c
Jiheon <chrpjh@gmail.com>
2025-09-26 16:58:54 +0900

    asdf

--- etc/add_dummy_resource.py
+++ etc/add_dummy_resource.py
@@ -1,12 +1,6 @@
 from kubernetes.config import load_kube_config
 from kubernetes.client import CoreV1Api
 
-node_list = [
-    "vnode2.pnode2.idc1.ten1010.io",
-    "vnode2.pnode7.idc1.ten1010.io",
-    "vnode1.pnode7.idc1.ten1010.io",
-    "vnode2.pnode6.idc1.ten1010.io"
-]
 
 load_kube_config()
 
@@ -17,12 +11,11 @@ body = {
         "capacity": {
             "ten1010.io/netif-ib-400g-sid1": 4,
             "ten1010.io/netif-ib-400g-sid2": 4,
-            "ten1010.io/netif-eth-100g-sid3": 4,
-            "ten1010.io/netif-eth-400g-sid4": 4,
-            "ten1010.io/netif-ib-400g-sid1": 4,
-            "ten1010.io/netif-ib-400g-sid1": 4,
-            "ten1010.io/netif-ib-400g-sid1": 4
+            "ten1010.io/netif-eth-100g-sid3": 1,
+            "ten1010.io/netif-eth-10g-sid4": 1,
         }
     }
 }
-api.patch_node_status(name="")
+node_name = "vnode2.pnode2.idc1.ten1010.io"
+
+api.patch_node_status(name=node_name, body=body)
--- src/main.py
+++ src/main.py
@@ -11,3 +11,23 @@ if __name__ == "__main__":
     load_models()
     controller = Controller()
     controller.run()
+
+
+
+
+
+
+"""
+
+Pkey는 네트워크 오퍼레이터랑 별개로 동작해야될 듯
+
+
+
+
+1. Nvidia 네트워크 오퍼레이터 설치
+2. NIC 서치해서 리스트 뽑음
+3. 위의 리스트에서 리소스로 쓸 것만 뽑아서 config로 만듦
+4. config를 기반으로 
+
+
+"""
